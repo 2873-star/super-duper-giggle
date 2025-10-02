70ccc19bcf8071089356f9ed763be069244e729f
2873-star <juwon2873@gmail.com>
2025-10-03 01:21:02 +0900

    Create Assets.kt & MultiTemplateMatch.kt

---
+++ Assets.kt & MultiTemplateMatch.kt
@@ -0,0 +1,15 @@
+package com.example.autoclicker
+
+import org.opencv.core.Mat
+
+class Assets {
+    fun getStairAssets(): List<Mat> {
+        return emptyList() // 실제 템플릿 로드
+    }
+}
+
+class MultiTemplateMatch {
+    fun predict(templates: List<Mat>, img: Mat): List<Any> {
+        return emptyList() // OpenCV 템플릿 매칭
+    }
+}
